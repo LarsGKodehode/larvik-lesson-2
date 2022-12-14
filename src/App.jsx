36eafadf585e204b8c9f012507d17083286cade7
LarsG <lars.ga.solheim@gmail.com>
2022-12-14 11:05:25 +0100

    seccond half of lesson

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,25 @@ import Card from './components/Card'
 // Data
 import { cardInfo } from './data/data'
 
+
+function handleClick(event) {
+  console.log(event)
+}
+
+function handleSimpleClick() {
+  console.log("I was clicked")
+}
+
 function App() {
   return (
     <>
       <main>
 
         {/* This is not the way! : Card() */}
+
+        <button onClick={(event) => handleClick(event)}>Click ME!</button>
+
+        <button onClick={handleSimpleClick}>Click ME!</button>
 
         {cardInfo.map(
           (element, index) => {
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,7 @@
+/**
+ * This is whats called a JSDoc string/comment
+ * Put information about how to use the component here
+ */
 function Card(props) {
   const { tittel, tekst } = props
 
